d226d6e158048103a31a33a87947027a1e55c3f7
YSCJustin <yscjustin@gmail.com>
2024-01-31 21:06:16 +0800

    Bored,

---
+++ messages.js
@@ -0,0 +1,134 @@
+let interval;
+let messages = [
+    {person: "Mok Tsz Lung",
+     message: `I half success half fail this event`},
+     {person: "Mok Tsz Lung",
+     message: "yo i win genshin"},
+     {person: "Mok Tsz Lung",
+     message: "frfr"},
+     {person: "Mok Tsz Lung",
+     message: "dim suen"},
+     {person: "Mok Tsz Lung",
+     message: "me very pro"},
+     {person: "Mok Tsz Lung",
+     message: "no u"},
+     {person: "Mok Tsz Lung",
+     message: ":()"},
+     {person: "Mok Tsz Lung",
+     message: "wacao"},
+     {person: "Mok Tsz Lung",
+     message: "YOOOOO"},
+     {person: "Mok Tsz Lung",
+     message: "yoooo"},
+     {person: "Mok Tsz Lung",
+     message: "hmm"},
+     {person: "Mok Tsz Lung",
+     message: "wtf"},
+     {person: "Mok Tsz Lung",
+     message: "why im so trasj"},
+     {person: "Mok Tsz Lung",
+     message: "how kau"},
+     {person: "Mok Tsz Lung",
+     message: "wtf dp"},
+     {person: "Mok Tsz Lung",
+     message: "u join i join"},
+     {person: "Mok Tsz Lung",
+     message: "omg"},
+     {person: "Mok Tsz Lung",
+     message: "no girl"},
+     {person: "Mok Tsz Lung",
+     message: "pls explain"},
+     {person: "Mok Tsz Lung",
+     message: "how come eng too pro"},
+     {person: "Mok Tsz Lung",
+     message: "just found that i bailan many days"},
+     {person: "Mok Tsz Lung",
+     message: "i express my admire to your sister"},
+     {person: "Mok Tsz Lung",
+     message: "felix hv perfect pitch"},
+     {person: "Mok Tsz Lung",
+     message: "no i cant yurk her out"},
+     {person: "Mok Tsz Lung",
+     message: "i saw at least 2 girls i know them but they dont know me"},
+     {person: "Mok Tsz Lung",
+     message: "hi my friend dragon wants to get your ig"},
+     {person: "Mok Tsz Lung",
+     message: "熟女殺手"},
+     {person: "Mok Tsz Lung",
+     message: "my desperate life"},
+     {person: "Mok Tsz Lung",
+     message: "wtf ada wong sdic noip both silver rank 1"},
+     {person: "Mok Tsz Lung",
+     message: "lets go anime girl"},
+     {person: "Mok Tsz Lung",
+     message: "omg positive contribution"},
+
+
+
+]
+let unsentMessages = [...messages];
+
+document.addEventListener('DOMContentLoaded', async function() {
+    let speed = 2000;
+    let speedInput = document.getElementById('speed');
+    interval = setInterval(function() {
+        if (unsentMessages.length === 0) {
+            unsentMessages = [...messages];
+        }
+
+        let randomIndex = Math.floor(Math.random() * unsentMessages.length);
+        let messageContent = unsentMessages[randomIndex].message;
+
+        unsentMessages.splice(randomIndex, 1);
+        let message = document.createElement('div');
+        message.className = 'message';
+        let content = document.createElement('p');
+        let author = document.createElement('p');
+        content.style.fontWeight='bolder'
+        content.textContent = messageContent;
+        author.textContent = unsentMessages[randomIndex].person;
+        message.appendChild(content);
+        message.appendChild(author);
+
+        let container = document.getElementById('messages');
+        container.insertBefore(message, container.firstChild);
+
+        if (container.childNodes.length > 20) {
+            container.removeChild(container.lastChild);
+        }
+    }, speed);
+    speedInput.addEventListener('input', function() {
+        let speedValue = speedInput.value;
+    
+        speed = speedValue;
+        clearInterval(interval);
+        interval = setInterval(function() {
+            if (unsentMessages.length === 0) {
+                unsentMessages = [...messages];
+            }
+    
+            let randomIndex = Math.floor(Math.random() * unsentMessages.length);
+            let messageContent = unsentMessages[randomIndex].message;
+    
+            unsentMessages.splice(randomIndex, 1);
+            let message = document.createElement('div');
+            message.className = 'message';
+            let content = document.createElement('p');
+            let author = document.createElement('p');
+            content.style.fontWeight='bolder'
+            content.textContent = messageContent;
+            author.textContent = unsentMessages[randomIndex].person;
+            message.appendChild(content);
+            message.appendChild(author);
+    
+            let container = document.getElementById('messages');
+            container.insertBefore(message, container.firstChild);
+    
+            if (container.childNodes.length > 20) {
+                container.removeChild(container.lastChild);
+            }
+        }, speed);
+    });
+
+
+});
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', async function() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -10,5 +13,33 @@ document.addEventListener('DOMContentLoaded', async function() {
     hidden.forEach((item) => {
         observer.observe(item);
     })
+    let messages = 0;
+
+// setInterval(function() {
+
+//     let message = document.createElement('div');
+//     message.className = 'message';
+//     let content = document.createElement('p');
+
+//     content.style.fontWeight='bolder'
+//     message.appendChild(content);
+//     let author = document.createElement('p');
+//     author.textContent= `Considering Mok Tsz Lung's apparent difficulties in comprehending basic English sentences, it becomes increasingly apparent that he may face challenges in navigating the demands of the modern world where English is widely utilized. The ability to understand and communicate effectively in English has become an essential skill set, enabling individuals to access a wealth of information, engage in global discourse, and establish meaningful connections with diverse cultures.Considering Mok Tsz Lung's apparent difficulties in comprehending basic English sentences, it becomes increasingly apparent that he may face challenges in navigating the demands of the modern world where English is widely utilized. The ability to understand and communicate effectively in English has become an essential skill set, enabling individuals to access a wealth of information, engage in global discourse, and establish meaningful connections with diverse cultures. Considering Mok Tsz Lung's apparent difficulties in comprehending basic English sentences, it becomes increasingly apparent that he may face challenges in navigating the demands of the modern world where English is widely utilized. The ability to understand and communicate effectively in English has become an essential skill set, enabling individuals to access a wealth of information, engage in global discourse, and establish meaningful connections with diverse cultures.Considering Mok Tsz Lung's apparent difficulties in comprehending basic English sentences, it becomes increasingly apparent that he may face challenges in navigating the demands of the modern world where English is widely utilized. The ability to understand and communicate effectively in English has become an essential skill set, enabling individuals to access a wealth of information, engage in global discourse, and establish meaningful connections with diverse cultures.`;
+//     message.appendChild(author);
+    
+
+
+//     let container = document.getElementById('messages');
+//     container.insertBefore(message, container.firstChild);
+//     content.textContent = 'Message ' + messages++;
+    
+//     if (container.childNodes.length >= 10) {
+//         let oldestMessage = container.lastChild;
+//             container.removeChild(oldestMessage);
+//     }
+  
+
+
+// },5000);  
 })
 
